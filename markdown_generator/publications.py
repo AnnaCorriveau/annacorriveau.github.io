e9a943f758a6ea6fe714234fd4d3d31b3a68052c
AnnaCorriveau <corriveauanna@gmail.com>
2024-11-12 16:13:52 -0600

    Update publications.py

--- markdown_generator/publications.py
+++ markdown_generator/publications.py
@@ -94,7 +94,7 @@ for row, item in publications.iterrows():
     ## Markdown description for individual page
     
     if len(str(item.paper_url)) > 5:
-        md += "\n\n<a href='" + item.paper_url + "'>Download paper here</a>" 
+        md += "\n\n<a href='" + item.paper_url + "'>Download paper here</a>\n" 
 
     if len(str(item.paper_url)) > 5:
         md += "\n<a href='" + item.journal_url + "'>Read paper here</a>\n" 
